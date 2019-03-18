16dc2c4e1e6dd6c4238326d6a0dd8492e153e610
Tuan PHAM <phamtuantkt@gmail.com>
2019-03-18 18:45:45 +0700

    First commit

---
+++ sourcecode.py
@@ -0,0 +1,296 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from sklearn import metrics
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+# from sklearn.neural_network import MLPClassifier
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.naive_bayes import MultinomialNB
+# from sklearn.linear_model import SGDClassifier
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.ensemble import AdaBoostClassifier
+# from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from pyvi import ViTokenizer
+import re
+import string
+import codecs
+
+#Từ điển tích cực, tiêu cực, phủ định
+path_nag = 'sentiment_dicts/nag.txt'
+path_pos = 'sentiment_dicts/pos.txt'
+path_not = 'sentiment_dicts/not.txt'
+
+with codecs.open(path_nag, 'r', encoding='UTF-8') as f:
+    nag = f.readlines()
+nag_list = [n.replace('\n', '') for n in nag]
+
+with codecs.open(path_pos, 'r', encoding='UTF-8') as f:
+    pos = f.readlines()
+pos_list = [n.replace('\n', '') for n in pos]
+with codecs.open(path_not, 'r', encoding='UTF-8') as f:
+    not_ = f.readlines()
+not_list = [n.replace('\n', '') for n in not_]
+
+
+VN_CHARS_LOWER = u'ạảãàáâậầấẩẫăắằặẳẵóòọõỏôộổỗồốơờớợởỡéèẻẹẽêếềệểễúùụủũưựữửừứíìịỉĩýỳỷỵỹđð'
+VN_CHARS_UPPER = u'ẠẢÃÀÁÂẬẦẤẨẪĂẮẰẶẲẴÓÒỌÕỎÔỘỔỖỒỐƠỜỚỢỞỠÉÈẺẸẼÊẾỀỆỂỄÚÙỤỦŨƯỰỮỬỪỨÍÌỊỈĨÝỲỶỴỸÐĐ'
+VN_CHARS = VN_CHARS_LOWER + VN_CHARS_UPPER
+def no_marks(s):
+    __INTAB = [ch for ch in VN_CHARS]
+    __OUTTAB = "a"*17 + "o"*17 + "e"*11 + "u"*11 + "i"*5 + "y"*5 + "d"*2
+    __OUTTAB += "A"*17 + "O"*17 + "E"*11 + "U"*11 + "I"*5 + "Y"*5 + "D"*2
+    __r = re.compile("|".join(__INTAB))
+    __replaces_dict = dict(zip(__INTAB, __OUTTAB))
+    result = __r.sub(lambda m: __replaces_dict[m.group(0)], s)
+    return result
+
+def normalize_text(text):
+
+    #Remove các ký tự kéo dài: vd: đẹppppppp
+    text = re.sub(r'([A-Z])\1+', lambda m: m.group(1).upper(), text, flags=re.IGNORECASE)
+
+    # Chuyển thành chữ thường
+    text = text.lower()
+
+    #Chuẩn hóa tiếng Việt, xử lý emoj, chuẩn hóa tiếng Anh, thuật ngữ
+    replace_list = {
+        'òa': 'oà', 'óa': 'oá', 'ỏa': 'oả', 'õa': 'oã', 'ọa': 'oạ', 'òe': 'oè', 'óe': 'oé','ỏe': 'oẻ',
+        'õe': 'oẽ', 'ọe': 'oẹ', 'ùy': 'uỳ', 'úy': 'uý', 'ủy': 'uỷ', 'ũy': 'uỹ','ụy': 'uỵ', 'uả': 'ủa',
+        'ả': 'ả', 'ố': 'ố', 'u´': 'ố','ỗ': 'ỗ', 'ồ': 'ồ', 'ổ': 'ổ', 'ấ': 'ấ', 'ẫ': 'ẫ', 'ẩ': 'ẩ',
+        'ầ': 'ầ', 'ỏ': 'ỏ', 'ề': 'ề','ễ': 'ễ', 'ắ': 'ắ', 'ủ': 'ủ', 'ế': 'ế', 'ở': 'ở', 'ỉ': 'ỉ',
+        'ẻ': 'ẻ', 'àk': u' à ','aˋ': 'à', 'iˋ': 'ì', 'ă´': 'ắ','ử': 'ử', 'e˜': 'ẽ', 'y˜': 'ỹ', 'a´': 'á',
+        "👹": "nagative", "👻": "positive", "💃": "positive",'🤙': ' positive ', '👍': ' positive ',
+        "💄": "positive", "💎": "positive", "💩": "positive","😕": "nagative", "😱": "nagative", "😸": "positive",
+        "😾": "nagative", "🚫": "nagative",  "🤬": "nagative","🧚": "positive", "🧡": "positive",'🐶':' positive ',
+        '👎': ' nagative ', '😣': ' nagative ','✨': ' positive ', '❣': ' positive ','☀': ' positive ',
+        '♥': ' positive ', '🤩': ' positive ', 'like': ' positive ', '💌': ' positive ',
+        '🤣': ' positive ', '🖤': ' positive ', '🤤': ' positive ', ':(': ' nagative ', '😢': ' nagative ',
+        '❤': ' positive ', '😍': ' positive ', '😘': ' positive ', '😪': ' nagative ', '😊': ' positive ',
+        '?': ' ? ', '😁': ' positive ', '💖': ' positive ', '😟': ' nagative ', '😭': ' nagative ',
+        '💯': ' positive ', '💗': ' positive ', '♡': ' positive ', '💜': ' positive ', '🤗': ' positive ',
+        '^^': ' positive ', '😨': ' nagative ', '☺': ' positive ', '💋': ' positive ', '👌': ' positive ',
+        '😖': ' nagative ', '😀': ' positive ', ':((': ' nagative ', '😡': ' nagative ', '😠': ' nagative ',
+        '😒': ' nagative ', '🙂': ' positive ', '😏': ' nagative ', '😝': ' positive ', '😄': ' positive ',
+        '😙': ' positive ', '😤': ' nagative ', '😎': ' positive ', '😆': ' positive ', '💚': ' positive ',
+        '✌': ' positive ', '💕': ' positive ', '😞': ' nagative ', '😓': ' nagative ', '️🆗️': ' positive ',
+        '😉': ' positive ', '😂': ' positive ', ':v': '  positive ', '=))': '  positive ', '😋': ' positive ',
+        '💓': ' positive ', '😐': ' nagative ', ':3': ' positive ', '😫': ' nagative ', '😥': ' nagative ',
+        '😃': ' positive ', '😬': ' 😬 ', '😌': ' 😌 ', '💛': ' positive ', '🤝': ' positive ', '🎈': ' positive ',
+        '😗': ' positive ', '🤔': ' nagative ', '😑': ' nagative ', '🔥': ' nagative ', '🙏': ' nagative ',
+        '🆗': ' positive ', '😻': ' positive ', '💙': ' positive ', '💟': ' positive ',
+        '😚': ' positive ', '❌': ' nagative ', '👏': ' positive ', ';)': ' positive ', '<3': ' positive ',
+        '🌝': ' positive ',  '🌷': ' positive ', '🌸': ' positive ', '🌺': ' positive ',
+        '🌼': ' positive ', '🍓': ' positive ', '🐅': ' positive ', '🐾': ' positive ', '👉': ' positive ',
+        '💐': ' positive ', '💞': ' positive ', '💥': ' positive ', '💪': ' positive ',
+        '💰': ' positive ',  '😇': ' positive ', '😛': ' positive ', '😜': ' positive ',
+        '🙃': ' positive ', '🤑': ' positive ', '🤪': ' positive ','☹': ' nagative ',  '💀': ' nagative ',
+        '😔': ' nagative ', '😧': ' nagative ', '😩': ' nagative ', '😰': ' nagative ', '😳': ' nagative ',
+        '😵': ' nagative ', '😶': ' nagative ', '🙁': ' nagative ',
+        ':))': '  positive ', ':)': ' positive ', 'ô kêi': ' ok ', 'okie': ' ok ', ' o kê ': ' ok ',
+        'okey': ' ok ', 'ôkê': ' ok ', 'oki': ' ok ', ' oke ':  ' ok ',' okay':' ok ','okê':' ok ',
+        ' tks ': u' cám ơn ', 'thks': u' cám ơn ', 'thanks': u' cám ơn ', 'ths': u' cám ơn ', 'thank': u' cám ơn ',
+        '⭐': 'star ', '*': 'star ', '🌟': 'star ', '🎉': u' positive ',
+        'kg ': u' không ','not': u' không ', u' kg ': u' không ', '"k ': u' không ',' kh ':u' không ','kô':u' không ','hok':u' không ',' kp ': u' không phải ',u' kô ': u' không ', '"ko ': u' không ', u' ko ': u' không ', u' k ': u' không ', 'khong': u' không ', u' hok ': u' không ',
+        'he he': ' positive ','hehe': ' positive ','hihi': ' positive ', 'haha': ' positive ', 'hjhj': ' positive ',
+        ' lol ': ' nagative ',' cc ': ' nagative ','cute': u' dễ thương ','huhu': ' nagative ', ' vs ': u' với ', 'wa': ' quá ', 'wá': u' quá', 'j': u' gì ', '“': ' ',
+        ' sz ': u' cỡ ', 'size': u' cỡ ', u' đx ': u' được ', 'dk': u' được ', 'dc': u' được ', 'đk': u' được ',
+        'đc': u' được ','authentic': u' chuẩn chính hãng ',u' aut ': u' chuẩn chính hãng ', u' auth ': u' chuẩn chính hãng ', 'thick': u' positive ', 'store': u' cửa hàng ',
+        'shop': u' cửa hàng ', 'sp': u' sản phẩm ', 'gud': u' tốt ','god': u' tốt ','wel done':' tốt ', 'good': u' tốt ', 'gút': u' tốt ',
+        'sấu': u' xấu ','gut': u' tốt ', u' tot ': u' tốt ', u' nice ': u' tốt ', 'perfect': 'rất tốt', 'bt': u' bình thường ',
+        'time': u' thời gian ', 'qá': u' quá ', u' ship ': u' giao hàng ', u' m ': u' mình ', u' mik ': u' mình ',
+        'ể': 'ể', 'product': 'sản phẩm', 'quality': 'chất lượng','chat':' chất ', 'excelent': 'hoàn hảo', 'bad': 'tệ','fresh': ' tươi ','sad': ' tệ ',
+        'date': u' hạn sử dụng ', 'hsd': u' hạn sử dụng ','quickly': u' nhanh ', 'quick': u' nhanh ','fast': u' nhanh ','delivery': u' giao hàng ',u' síp ': u' giao hàng ',
+        'beautiful': u' đẹp tuyệt vời ', u' tl ': u' trả lời ', u' r ': u' rồi ', u' shopE ': u' cửa hàng ',u' order ': u' đặt hàng ',
+        'chất lg': u' chất lượng ',u' sd ': u' sử dụng ',u' dt ': u' điện thoại ',u' nt ': u' nhắn tin ',u' tl ': u' trả lời ',u' sài ': u' xài ',u'bjo':u' bao giờ ',
+        'thik': u' thích ',u' sop ': u' cửa hàng ', ' fb ': ' facebook ', ' face ': ' facebook ', ' very ': u' rất ',u'quả ng ':u' quảng  ',
+        'dep': u' đẹp ',u' xau ': u' xấu ','delicious': u' ngon ', u'hàg': u' hàng ', u'qủa': u' quả ',
+        'iu': u' yêu ','fake': u' giả mạo ', 'trl': 'trả lời', '><': u' positive ',
+        ' por ': u' tệ ',' poor ': u' tệ ', 'ib':u' nhắn tin ', 'rep':u' trả lời ',u'fback':' feedback ','fedback':' feedback ',
+        #dưới 3* quy về 1*, trên 3* quy về 5*
+        '6 sao': ' 5star ','6 star': ' 5star ', '5star': ' 5star ','5 sao': ' 5star ','5sao': ' 5star ',
+        'starstarstarstarstar': ' 5star ', '1 sao': ' 1star ', '1sao': ' 1star ','2 sao':' 1star ','2sao':' 1star ',
+        '2 starstar':' 1star ','1star': ' 1star ', '0 sao': ' 1star ', '0star': ' 1star ',}
+
+    for k, v in replace_list.items():
+        text = text.replace(k, v)
+
+    # chuyen punctuation thành space
+    translator = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+    text = text.translate(translator)
+
+    text = ViTokenizer.tokenize(text)
+    texts = text.split()
+    len_text = len(texts)
+
+    texts = [t.replace('_', ' ') for t in texts]
+    for i in range(len_text):
+        cp_text = texts[i]
+        if cp_text in not_list: # Xử lý vấn đề phủ định (VD: áo này chẳng đẹp--> áo này notpos)
+            numb_word = 2 if len_text - i - 1 >= 4 else len_text - i - 1
+
+            for j in range(numb_word):
+                if texts[i + j + 1] in pos_list:
+                    texts[i] = 'notpos'
+                    texts[i + j + 1] = ''
+
+                if texts[i + j + 1] in nag_list:
+                    texts[i] = 'notnag'
+                    texts[i + j + 1] = ''
+        else: #Thêm feature cho những sentiment words (áo này đẹp--> áo này đẹp positive)
+            if cp_text in pos_list:
+                texts.append('positive')
+            elif cp_text in nag_list:
+                texts.append('nagative')
+
+    text = u' '.join(texts)
+
+    #remove nốt những ký tự thừa thãi
+    text = text.replace(u'"', u' ')
+    text = text.replace(u'️', u'')
+    text = text.replace('🏻','')
+    return text
+
+class DataSource(object):
+
+    def _load_raw_data(self, filename, is_train=True):
+
+        a = []
+        b = []
+
+        regex = 'train_'
+        if not is_train:
+            regex = 'test_'
+
+        with open(filename, 'r') as file:
+            for line in file:
+                if regex in line:
+                    b.append(a)
+                    a = [line]
+                elif line != '\n':
+                    a.append(line)
+        b.append(a)
+
+        return b[1:]
+
+    def _create_row(self, sample, is_train=True):
+
+        d = {}
+        d['id'] = sample[0].replace('\n', '')
+        review = ""
+
+        if is_train:
+            for clause in sample[1:-1]:
+                review += clause.replace('\n', ' ')
+                review = review.replace('.', ' ')
+
+            d['label'] = int(sample[-1].replace('\n', ' '))
+        else:
+            for clause in sample[1:]:
+                review += clause.replace('\n', ' ')
+                review = review.replace('.', ' ')
+
+
+        d['review'] = review
+
+        return d
+
+    def load_data(self, filename, is_train=True):
+
+        raw_data = self._load_raw_data(filename, is_train)
+        lst = []
+
+        for row in raw_data:
+            lst.append(self._create_row(row, is_train))
+
+        return lst
+
+    def transform_to_dataset(self, x_set,y_set):
+        X, y = [], []
+        for document, topic in zip(list(x_set), list(y_set)):
+            document = normalize_text(document)
+            X.append(document.strip())
+            y.append(topic)
+            #Aumentation bằng cách remove dấu tiếng Việt
+            # X.append(no_marks(document))
+            # y.append(topic)
+        return X, y
+
+ds = DataSource()
+train_data = pd.DataFrame(ds.load_data('data_clean/train.crash'))
+new_data = []
+
+#Thêm mẫu bằng cách lấy trong từ điển Sentiment (nag/pos)
+for index,row in enumerate(nag_list):
+    new_data.append(['pos'+str(index),'0',row])
+for index,row in enumerate(nag_list):
+    new_data.append(['nag'+str(index),'1',row])
+
+new_data = pd.DataFrame(new_data,columns=list(['id','label','review']))
+train_data.append(new_data)
+test_data = pd.DataFrame(ds.load_data('data_clean/test.crash', is_train=False))
+
+#Try some models
+classifiers = [
+            # MultinomialNB(),
+            # DecisionTreeClassifier(),
+            LogisticRegression(),
+            # SGDClassifier(),
+            LinearSVC(fit_intercept = True,multi_class='crammer_singer', C=1),
+        ]
+
+X_train, X_test, y_train, y_test = train_test_split(train_data.review, train_data.label, test_size=0.3,random_state=42)
+X_train, y_train = ds.transform_to_dataset(X_train,y_train)
+X_test, y_test = ds.transform_to_dataset(X_test, y_test)
+
+#THÊM STOPWORD LÀ NHỮNG TỪ KÉM QUAN TRỌNG
+stop_ws = (u'rằng',u'thì',u'là',u'mà')
+
+for classifier in classifiers:
+    steps = []
+    steps.append(('CountVectorizer', CountVectorizer(ngram_range=(1,5),stop_words=stop_ws,max_df=0.5, min_df=5)))
+    steps.append(('tfidf', TfidfTransformer(use_idf=False, sublinear_tf = True,norm='l2',smooth_idf=True)))
+    steps.append(('classifier', classifier))
+    clf = Pipeline(steps)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    report1 = metrics.classification_report(y_test, y_pred, labels=[1,0], digits=3)
+
+X_train, y_train = ds.transform_to_dataset(train_data.review, train_data.label)
+
+
+#TRAIN OVERFITTING/ERRO ANALYSIS
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_train)
+report2 = metrics.classification_report(y_train, y_pred, labels=[1,0], digits=3)
+
+#ERRO ANALYSIS
+for id,x, y1, y2 in zip(train_data.id, X_train, y_train, y_pred):
+    if y1 != y2:
+        # CHECK EACH WRONG SAMPLE POSSITIVE/NAGATIVE
+        if y1!=1:#0:
+            print(id,x, y1, y2)
+
+#CROSS VALIDATION
+cross_score = cross_val_score(clf, X_train,y_train, cv=5)
+
+#REPORT
+print('DATASET LEN %d'%(len(X_train)))
+print('TRAIN 70/30 \n\n',report1)
+print('TRAIN OVERFITING\n\n',report2)
+print("CROSSVALIDATION 5 FOLDS: %0.4f (+/- %0.4f)" % (cross_score.mean(), cross_score.std() * 2))
+
+# #SAVE FILE SUBMIT
+test_list = []
+for document in test_data.review:
+    document = normalize_text(document)
+    test_list.append(document)
+y_predict = clf.predict(test_list)
+test_data['label'] = y_predict
+# test_data['content'] = test_list
+test_data = test_data.sort_values(by=['label'])
+test_data[['id', 'label']].to_csv('submit.csv', index=False)
